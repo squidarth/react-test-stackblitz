89ccb461a5a9590661debf0856d3b0d570f4a8e9
squidarth <sshanker220@gmail.com>
2022-05-19 18:57:42 -0400

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Making a new webite!</h1>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-3j5mo2
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-3j5mo2)
